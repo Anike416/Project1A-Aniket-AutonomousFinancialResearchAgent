05b598f22fab45093b6217303cd1cdb5cc39fdfd
Aniket Nainwal <178406663+Anike416@users.noreply.github.com>
2026-05-11 22:48:11 +0530

    Delete sample.js

--- sample.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-
-let dataBuffer = fs.readFileSync('463548A_Agentic-AI_Autonomous_Financial_Research_Agent.docx.pdf');
-
-pdf(dataBuffer).then(function(data) {
-    fs.writeFileSync("output.txt", data.text);
-});
